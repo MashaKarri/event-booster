a545ba043a164d969354d57a35285ffa3bd7420d
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2026-01-06 19:56:37 +0200

    start

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./js/header";
+import "./js/main-section-api";
+import "./js/main-section-layout";
+import "./js/main-section-pagination";
+import "./js/main-modal-api";
+import "./js/main-modal-layout";
